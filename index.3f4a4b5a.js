f0c69052efe35ca433b3f39c842c82ab722d0d29
Harper Davis <harperkdavis@gmail.com>
2024-01-07 16:57:37 -0800

    Relative paths

---
+++ index.3f4a4b5a.js
@@ -0,0 +1,3 @@
+new URLSearchParams(window.location.search).get("editor");//# sourceMappingURL=index.3f4a4b5a.js.map
+
+//# sourceMappingURL=index.3f4a4b5a.js.map
---
+++ index.f8c720f7.js
@@ -0,0 +1,3 @@
+localStorage.getItem("tab");//# sourceMappingURL=index.f8c720f7.js.map
+
+//# sourceMappingURL=index.f8c720f7.js.map
